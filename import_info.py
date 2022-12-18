664c41660cef4c58eb761e857bd1092879111baa
Tatiana <tt_tanya_88@mail.ru>
2022-12-18 15:51:50 +0300

    import

---
+++ import_info.py
@@ -0,0 +1,8 @@
+import csv
+def create_data(text: str):
+
+    text = list(text.split())
+    with open('data_base.csv', 'a', newline='', encoding='UTF-8') as file:
+        data = csv.writer(file)
+        data.writerow(text)
+        print('Информация внесена')
